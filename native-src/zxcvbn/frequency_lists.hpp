22f045d352f7ddee128efb3556e97bb9d8f9f3c6
Rian Hunter <rian@alum.mit.edu>
2016-06-08 20:47:24 -0700

    Modify build_frequency_lists.py to also generate C++

--- native-src/zxcvbn/frequency_lists.hpp
+++ native-src/zxcvbn/frequency_lists.hpp
@@ -1,21 +1,16 @@
 #ifndef __ZXCVBN__FREQUENCY_LISTS_HPP
 #define __ZXCVBN__FREQUENCY_LISTS_HPP
 
-#include <string>
-#include <unordered_map>
-#include <vector>
+#include <zxcvbn/frequency_lists_common.hpp>
+#include <zxcvbn/_frequency_lists.hpp>
 
+#include <unordered_map>
+
+#include <cstdint>
 
 namespace zxcvbn {
 
-// XXX: this will be auto generated
-enum class DictionaryTag {
-  PASSWORDS,
-  ENGLISH_WIKIPEDIA,
-  SURNAMES,
-  MALE_NAMES,
-  FEMALE_NAMES,
-};
+using DictionaryTag = _frequency_lists::DictionaryTag;
 
 }
 
@@ -32,23 +27,9 @@ struct hash<zxcvbn::DictionaryTag> {
 
 namespace zxcvbn {
 
-using rank_t = std::size_t;
-using RankedDict = std::unordered_map<std::string, rank_t>;
-
-template<class T = std::initializer_list<const char *>>
-RankedDict build_ranked_dict(const T & ordered_list) {
-  RankedDict result;
-  rank_t idx = 1; // rank starts at 1, not 0
-  for (const auto & word : ordered_list) {
-    result.insert(std::make_pair(word, idx));
-    idx += 1;
-  }
-  return result;
-}
-
 using RankedDicts = std::unordered_map<DictionaryTag, const RankedDict &>;
 
-RankedDicts convert_to_ranked_dicts(std::unordered_map<DictionaryTag, RankedDict> &);
+RankedDicts convert_to_ranked_dicts(std::unordered_map<DictionaryTag, RankedDict> & ranked_dicts);
 RankedDicts default_ranked_dicts();
 
 }
